f0ec2b2c84db09d4f4cec6e50f077e649e222f76
nNON <nn0n@duck.com>
2023-03-02 20:59:57 -0700

    refactor: removed runtime for prod

--- src/store.ts
+++ src/store.ts
@@ -1,11 +1,11 @@
-import { ComposeClient } from '@composedb/client'
-import { JsonRpcProvider } from '@ethersproject/providers'
+// import { ComposeClient } from '@composedb/client'
+// import { JsonRpcProvider } from '@ethersproject/providers'
 import { create } from 'zustand'
 
-import { definition } from '../out/__generated__/runtime'
+// import { definition } from '../out/__generated__/runtime'
 
 export const useStore = create(() => ({
-  compose: new ComposeClient({ ceramic: process.env.NEXT_CERAMIC_URL || 'http://localhost:7007', definition }),
+  // compose: new ComposeClient({ ceramic: process.env.NEXT_CERAMIC_URL || 'http://localhost:7007', definition }),
   address: `0x${'00000000000'}`,
   blockNumber: 0,
   blockHash: '',
